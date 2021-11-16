c67ee4acafe94a293fd7d171845dc71dac76da4f
Victor Giovannoni <vernalhav@usp.br>
2021-11-15 23:59:53 -0300

    Refactor init method creation, remove init header type hints

--- metademo/mydataclass.py
+++ metademo/mydataclass.py
@@ -1,30 +1,31 @@
+from typing import Any, Dict, List
+from pprint import pprint
+
 
 class MyDataClassMeta(type):
     def __new__(cls, name, bases, namespace):
-
         attributes = namespace.get('__annotations__', {})
 
-        indent = '\n\t'
-        init_signature = f"self{''.join(f', {attr}: {attr_type.__name__}' for attr, attr_type in attributes.items())}"
-        init_body = f"{indent}{indent.join(f'self.{attr} = {attr}' for attr in attributes) or 'pass'}"
-
-        init_method = f'''
-def __init__({init_signature}):
-{init_body}
-'''
-
+        init_method = MyDataClassMeta.__get_init_method(attributes)
+        pprint(globals())
         exec(init_method, globals(), namespace)
 
         return super().__new__(cls, name, bases, namespace)
 
 
+    @staticmethod
+    def __get_function_string(name, args: List[str], body: List[str]) -> str:        
+        signature = ', '.join(args)
+        body_content = '\n\t'.join(body) or 'pass'
+        return f'def {name}({signature}):\n\t{body_content}'
+
+
+    @staticmethod
+    def __get_init_method(attributes: Dict[str, type]) -> str:
+        init_params = ['self'] + list(attributes.keys())
+        body = [ f'self.{arg} = {arg}' for arg in attributes ]
+        return MyDataClassMeta.__get_function_string('__init__', init_params, body)
+
+
 class MyDataClass(metaclass=MyDataClassMeta):
     pass
-
-
-class A(MyDataClass):
-    size: int
-    color: str
-
-    def test(x: int) -> str:
-        pass
--- tests/test_mydataclass.py
+++ tests/test_mydataclass.py
@@ -1,35 +1,55 @@
-from metademo.mydataclass import MyDataClass
+from typing import List
+
 import pytest
+from metademo.mydataclass import MyDataClass
+
+
+class Person(MyDataClass):
+    name: str
+    age: int
+
+    def greet(self):
+        return f'Hello, {self.name}!'
+
+
+class Empty(MyDataClass):
+    pass
+
+
+class NotPrimitiveType(MyDataClass):
+    people: List[Person]
+    empty: Empty
 
 
 class TestMyDataClass():
-    
-    class Person(MyDataClass):
-        name: str
-        age: int
-
-    class Empty(MyDataClass):
-        pass
-
     @staticmethod
     def test_bad_instantiation():
         with pytest.raises(TypeError):
-            p = TestMyDataClass.Person()
+            p = Person()
 
     @staticmethod
     def test_empty_insantiation():
-        e = TestMyDataClass.Empty()
+        e = Empty()
 
     @staticmethod
     def test_attributes():        
-        p = TestMyDataClass.Person('William', 22)
+        p = Person('William', 22)
         assert p.name == 'William'
         assert p.age == 22
     
     @staticmethod
-    def test_multiple_instances():
-        plato = TestMyDataClass.Person('Plato', 2442)
-        aristotle = TestMyDataClass.Person('Aristotle', 2405)
+    def test_method():
+        p = Person('William', 22)
+        assert p.greet() == 'Hello, William!'
 
+    @staticmethod
+    def test_multiple_instances():
+        plato = Person('Plato', 2442)
+        aristotle = Person('Aristotle', 2405)
         assert plato.name == 'Plato' and plato.age == 2442
         assert aristotle.name == 'Aristotle' and aristotle.age == 2405
+
+    @staticmethod
+    def test_not_primitive_type():
+        not_primitive = NotPrimitiveType([Person('Tijolo', 21), Person('Baracus', 23), Person('Jão', 21)], Empty())
+        assert not_primitive.people[2].name == 'Jão'
