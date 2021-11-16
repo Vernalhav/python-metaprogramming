e6fa43f9ef20df251e8e2e108cab9ff32b41806e
Victor Giovannoni <vernalhav@usp.br>
2021-11-16 10:46:57 -0300

    [WIP] annotations not working with nested namespaced types

--- metademo/mydataclass.py
+++ metademo/mydataclass.py
@@ -1,30 +1,37 @@
-from typing import Any, Dict, List
-from pprint import pprint
-
+import pprint
+from typing import Dict, List, Tuple, get_type_hints
+import inspect
+import sys
 
 class MyDataClassMeta(type):
     def __new__(cls, name, bases, namespace):
         attributes = namespace.get('__annotations__', {})
-
-        init_method = MyDataClassMeta.__get_init_method(attributes)
-        pprint(globals())
-        exec(init_method, globals(), namespace)
+        init_method = MyDataClassMeta.__get_init_method(attributes, namespace.get('__module__'))
+        print(f'\n========================\n{init_method}\n===================\n\n\n')
+        exec(init_method, sys.modules[namespace.get('__module__')].__dict__, namespace)
 
         return super().__new__(cls, name, bases, namespace)
 
+    @staticmethod
+    def __annotate_in_module(base_module: str):
+        def __annotate_parameter(param: Tuple[str, type]) -> List[str]:
+            param_name, param_type = param
+            return f'{param_name}' + (f': {inspect.formatannotation(param_type, base_module)}' if param_type is not None else '')
+        return __annotate_parameter
 
     @staticmethod
-    def __get_function_string(name, args: List[str], body: List[str]) -> str:        
-        signature = ', '.join(args)
+    def __get_function_string(name, args: Dict[str, type], body: List[str], base_module: str=None) -> str:
+        annotator_function = MyDataClassMeta.__annotate_in_module(base_module)
+        annotated_params = map(annotator_function, args.items())
+        signature = ', '.join(annotated_params)
+
         body_content = '\n\t'.join(body) or 'pass'
         return f'def {name}({signature}):\n\t{body_content}'
 
-
-    @staticmethod
-    def __get_init_method(attributes: Dict[str, type]) -> str:
-        init_params = ['self'] + list(attributes.keys())
+    def __get_init_method(attributes: Dict[str, type], base_module: str=None) -> str:
         body = [ f'self.{arg} = {arg}' for arg in attributes ]
-        return MyDataClassMeta.__get_function_string('__init__', init_params, body)
+        init_params = { 'self': None, **attributes }
+        return MyDataClassMeta.__get_function_string('__init__', init_params, body, base_module)
 
 
 class MyDataClass(metaclass=MyDataClassMeta):
