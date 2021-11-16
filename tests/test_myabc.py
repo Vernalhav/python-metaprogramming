6bc9c3ccd12f7ac17718010094fd67ec2a131e4f
Victor Giovannoni <vernalhav@usp.br>
2021-11-16 11:28:19 -0300

    Put auxiliary testing classes outside of main test class

--- tests/test_myabc.py
+++ tests/test_myabc.py
@@ -1,38 +1,35 @@
 import pytest
 from metademo.myabc import MyABC, myabstractmethod, AbstractClassInstantiationException
 
+class Interface(MyABC):
+    @myabstractmethod
+    def f(self):
+        pass
+
+    def identity(self, x):
+        return x
+
+class ConcreteClassA(Interface):
+    def f(self):
+        return 42
+
+class ConcreteClassB(Interface):
+    def f(self):
+        return 69
+
+class InterfaceSubclass(Interface):
+    pass
+
+class ConcreteClassC(InterfaceSubclass):
+    def f(self):
+        return 37
 
 class TestMyABC():
-
-    class Interface(MyABC):
-        @myabstractmethod
-        def f(self):
-            pass
-
-        def identity(self, x):
-            return x
-
-    class ConcreteClassA(Interface):
-        def f(self):
-            return 42
-
-    class ConcreteClassB(Interface):
-        def f(self):
-            return 69
-
-    class InterfaceSubclass(Interface):
-        pass
-    
-    class ConcreteClassC(InterfaceSubclass):
-        def f(self):
-            return 37
-
-
     @staticmethod
     def test_abstract_method_call():
-        a = TestMyABC.ConcreteClassA()
-        b = TestMyABC.ConcreteClassB()
-        c = TestMyABC.ConcreteClassC()
+        a = ConcreteClassA()
+        b = ConcreteClassB()
+        c = ConcreteClassC()
 
         assert a.f() == 42
         assert b.f() == 69
@@ -41,17 +38,17 @@ class TestMyABC():
     @staticmethod
     def test_abstract_instantiation_prevention():
         with pytest.raises(AbstractClassInstantiationException):
-            i = TestMyABC.Interface()
+            i = Interface()
     
     @staticmethod
     def test_not_implemented_instantiation_prevention():
         with pytest.raises(AbstractClassInstantiationException):
-            i = TestMyABC.InterfaceSubclass()
+            i = InterfaceSubclass()
 
     @staticmethod
     def test_nonabstract_base_method():
-        a = TestMyABC.ConcreteClassA()
-        b = TestMyABC.ConcreteClassB()
+        a = ConcreteClassA()
+        b = ConcreteClassB()
 
         assert a.identity(420) == 420
         assert b.identity(420) == 420
